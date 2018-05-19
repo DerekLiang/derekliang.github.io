c5a6327310ce5fe73e975aa67ed8acdf5a6bd721
Derek Liang <derek.liang.ca@gmail.com>
2018-05-18 17:57:39 -0700

    deploy

--- _deploy.bat
+++ _deploy.bat
@@ -1,1 +1,1 @@
-"c:\Program Files\Git\bin\bash.exe" -c "(git add -A && git commit -m deploy && git push); rm -rf .deploy_git && git clone git@github.com:DerekLiang/derekliang.github.io.git .deploy_git  && hexo generate &&  cp -vr public/** .deploy_git && hexo deploy"
+"c:\Program Files\Git\bin\bash.exe" -c "(git add -A && git commit -m deploy && git push); rm -rf .deploy_git && git clone git@github.com:DerekLiang/derekliang.github.io.git .deploy_git  && hexo generate -d"
